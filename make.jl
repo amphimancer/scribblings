28f19b03145f6b6b4194d36e318dbe2a48b2463f
Amphi <amphimancer@gmail.com>
2025-03-31 12:45:35 +0200

    adding March 30 story

--- make.jl
+++ make.jl
@@ -5,8 +5,9 @@ makedocs(
     authors="Amphimancer",
     pages = [
         "Hello" => "index.md"
-        #"2025" => Any[
-        #]
+        "2025" => Any[
+            "Hell's Administrator" => "content/2025/march/30.md"
+        ]
     ],
 )
 
