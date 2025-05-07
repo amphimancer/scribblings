8ba93f4accd8119ebd0483086faaafc18eabf5d2
Amphi <amphimancer@gmail.com>
2025-05-07 16:11:44 +0200

    adding new stories from today

--- make.jl
+++ make.jl
@@ -7,13 +7,15 @@ makedocs(
         "Hello" => "index.md"
         "2025" => Any[
             "May" => Any[
-                "A Reflection on the Wall" => "content/2025/may/6.md"
+                "My *Nature* Paper" => "content/2025/may/7-2.md"
+                "There's Something in the Water" => "content/2025/may/7.md"
+                "My Reflection Is Not Mine" => "content/2025/may/6.md"
             ]
             "April" => Any[
                 "Australia" => "content/2025/april/9.md"
                 "The Amphimancer: Not Alone" => "content/2025/april/8.md"
                 "A Walk Down Memory Lane" => "content/2025/april/7.md"
-                "Magic is Outside the Norm" => "content/2025/april/4.md"
+                "Magic Is Outside the Norm" => "content/2025/april/4.md"
                 "A Friend No More" => "content/2025/april/2.md"
                 "The Devil's Reincarnation" => "content/2025/april/1.md"
              ]
